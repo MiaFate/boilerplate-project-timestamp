e5a97e8d5cd25359e36bcdc7bb6368b12dd16986
miafate <miapurplefroot@gmail.com>
2024-01-13 14:55:39 -0300

    project timestamp test

--- index.js
+++ index.js
@@ -8,25 +8,44 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function(req, res) {
+  const date = req.params.date || ""
+  if (date.length === 10) {
+    const utc = new Date(date).toUTCString()
+    const unix = parseInt(new Date(date).getTime())
+    utc === "Invalid Date" ? res.json({ error: utc }) : res.json({ time: utc, unix })
+  } else if (!!parseInt(date)) {
+    const unix = parseInt((date))
+    const utc = new Date(date * 1000).toUTCString()
+    utc === "Invalid Date" ? res.json({ error: utc }) : res.json({ unix, time: utc })
+  } else if (date.length === 0) {
+    const time = new Date()
+
+    const utc = time.toUTCString()
+    const unix = parseInt(time.getTime())
+    utc === "Invalid Date" ? res.json({ error: utc }) : res.json({ time: utc, unix })
+  }
+  res.json({ error: "Invalid Date" })
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
